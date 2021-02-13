594afd10e93b1e3f53609f0b0ae1d8c8771e3f09
mohnishkheraa <mohnishkhera@ltts.com>
2021-02-13 06:00:52 +0530

    Create main.cpp

---
+++ Implementation/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include "convert.h"
+using namespace std;
+
+int main()
+{
+    //This program calculates ccgpa
+    int n; //This is number of subjects
+    
+    cout<<"Enter the number of subjects ";
+    
+    cin>>n;
+    
+    convert(n);
+
+}
