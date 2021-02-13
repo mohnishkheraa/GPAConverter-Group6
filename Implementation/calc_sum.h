043dacef3ea6d73ea084144453f6a010056cc210
Sarveshltts <78848824+Sarveshltts@users.noreply.github.com>
2021-02-13 11:52:09 +0530

    Calculator sum Function
    sum

---
+++ Implementation/calc_sum.h
@@ -0,0 +1,14 @@
+#include "calc_gpa.h"
+#include <iostream>
+using namespace std;
+int calc_sum(int arr[],int n){//this function calculates the sum of all the gpa
+    float gpasum; //sum of gpa's of all the subjects
+    
+    for(int i=0;i<n;i++){ //loop to calculate the sum
+        gpasum=gpasum+arr[i];
+    }
+    
+    calc_gpa(gpasum,n);
+    return 0;
+
+}
