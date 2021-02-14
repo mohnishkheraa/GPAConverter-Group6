b62208f5d18e5b18e464484d46e99959af584dae
Sarveshltts <78848824+Sarveshltts@users.noreply.github.com>
2021-02-14 12:35:43 +0530

    Create test_main.cpp

---
+++ Implementation/test/test_main.cpp
@@ -0,0 +1,16 @@
+#include "unity.h"
+#include "calc_gpa.h"
+
+void setUp() {}
+
+void tearDown() {}
+
+void main(void) {
+
+    UNITY_BEGIN();
+
+    test_calc_gpa();
+
+	return UNITY_END();
+    return 0;
+}
